15513493e25258ddd481460e22f4fbc325c8eff1
KatarinaRS <katarina.roser@gmail.com>
2022-03-31 20:23:54 +0200

    Multiplying decimals

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+    // only change code below this line
+    var product = numOne * numTwo
+    // only change code abowe this line
+  return product;
+  }
+  
+console.log(main(2.5, 2.0)); // change this line
+console.log(main(5.5, 5.5)); // change this line
+console.log(main(100.25, 43.2)); // change this line
+  module.exports = main;
