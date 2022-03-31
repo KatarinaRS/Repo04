53c7916f38fc71f2bf635c8f5dfdba31771e7818
KatarinaRS <katarina.roser@gmail.com>
2022-03-31 21:19:18 +0200

    Augmented addition and subtraction

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -3,9 +3,9 @@ function main(){
      var myX = 12;
      var myY = 19;
      var myZ = 24;
-     myX = myX += 3;
-     myY = myY += 17;
-     myZ = myZ -= 20; 
+     myX += 3;
+     myY += 17;
+     myZ -= 20; 
     // only change code abowe this line
   return {
       myX,
